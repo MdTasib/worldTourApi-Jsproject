c5f0332f55ecc41db3c128f5e7a7b9f822630560
ohidul alam <mdtasib442@gmali.com>
2021-02-06 19:05:19 +0600

    project done

--- index.js
+++ index.js
@@ -42,18 +42,37 @@ fetch(`https://restcountries.eu/rest/v2/all`)
 // (3)
 const displayData = countrys => {
     const countrysContainer = document.getElementById('countrys');
-    for (let i = 0; i < countrys.length; i++) {
-        const country = countrys[i];
 
+    // for loop
+    // for (let i = 0; i < countrys.length; i++) {
+    //     const country = countrys[i];
+    // }
+
+    // forEach loop
+    countrys.forEach(country => {
         const singleCountryDiv = document.createElement('div');
         singleCountryDiv.classList = 'country';
         const singleCountryInfo = `
         <h3 class="country-name">${country.name}</h3>
         <p>${country.capital}</p>
+        <button onclick="details('${country.name}')">Details</button>
         `
         singleCountryDiv.innerHTML = singleCountryInfo;
         countrysContainer.appendChild(singleCountryDiv);
+    })
+}
+const details = name => {
+    fetch(`https://restcountries.eu/rest/v2/name/${name}`)
+        .then(response => response.json())
+        .then(data => countryDetails(data[0]))
+}
 
-
-    }
+const countryDetails = country => {
+    const countryInfo = document.getElementById('country-info');
+    countryInfo.innerHTML = `
+    <h3>Country Name: ${country.name}</h3>
+    <p>Region: ${country.region}</p>
+    <p>Area: ${country.area}</p>
+    <img src="${country.flag}">
+    `
 }
